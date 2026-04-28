f33efc9ca9462c4ed01f4b9744b1fed030c3830a
itsmepraks <53210833+itsmepraks@users.noreply.github.com>
2026-04-28 09:14:00 -0400

    feat(ui): export package entrypoint

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,56 @@
+export { cn } from "./lib/utils";
+export { Button, buttonVariants } from "./components/button";
+export type { ButtonProps } from "./components/button";
+export {
+  Input,
+  InputField,
+  inputRootVariants,
+} from "./components/input";
+export type { InputProps, InputFieldProps } from "./components/input";
+export { Textarea, textareaVariants } from "./components/textarea";
+export type { TextareaProps } from "./components/textarea";
+export { Checkbox } from "./components/checkbox";
+export { RadioGroup, RadioGroupItem } from "./components/radio-group";
+export { Switch, switchVariants } from "./components/switch";
+export type { SwitchProps } from "./components/switch";
+export { Slider } from "./components/slider";
+export type { SliderProps } from "./components/slider";
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+} from "./components/select";
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "./components/tabs";
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./components/breadcrumb";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationEllipsis,
+} from "./components/pagination";
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./components/tooltip";
